288bcb36dd5c6140bf0d72ecc38fd8bbbd3532db
Matt Watson <mattwat@gmail.com>
2021-03-07 11:16:32 -0500

    add missing useEffect dependency

--- src/components/notifications/notification.js
+++ src/components/notifications/notification.js
@@ -64,7 +64,7 @@ export const Notification = ({ message }) => {
       const closeTimer = setTimeout(() => closeNotification(message.id), timeout)
       return () => clearTimeout(closeTimer)
     }
-  }, [closeNotification, message.autoClose, message.id])
+  }, [closeNotification, message.autoClose, message.id, timeout])
 
   return (
     <Wrapper onClick={ close } color={ colors[message.type] }>
