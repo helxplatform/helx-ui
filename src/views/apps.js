1b58107ae6ac2b8d5b7a2e02211a45976f7d11d4
Bo Zhou <peterbozhou@gmail.com>
2021-03-11 15:05:46 -0500

    Configure app listing to load from /api/v1 and remove hardcoded response

--- src/views/apps.js
+++ src/views/apps.js
@@ -187,7 +187,7 @@ const ServiceCard = ({ name, docs, sid, fqsid, creation_time, cpu, gpu, memory }
       <Card.Header><AppHeader>{name} <Status class><RunningStatus />Running</Status></AppHeader></Card.Header>
       <Relative>
         <Card.Body>
-          <Paragraph style={{ display: 'flex', fontSize: 20, justifyContent: 'space-between'}}><span>CPU: {cpu}</span> <span>GPU: {gpu}</span> <span>Memory: {memory}</span></Paragraph>
+          <Paragraph style={{ display: 'flex', fontSize: 20, justifyContent: 'space-between' }}><span>CPU: {cpu}</span> <span>GPU: {gpu}</span> <span>Memory: {memory}</span></Paragraph>
           <Paragraph>Creation Time: {creation_time}</Paragraph>
           <Link to={docs}>App Documentation</Link>
         </Card.Body>
@@ -210,98 +210,38 @@ export const Apps = () => {
   const [services, setServices] = useState([]);
   const [active, setActive] = useState('Apps');
 
-  const serviceResponse = [
-    {
-      "name": "Cloud Top",
-      "docs": "https://helxplatform.github.io/cloudtop-docs/",
-      "sid": "da600a3dd98e4b5ea3cfb5b63ae66732",
-      "fqsid": "cloud-top-da600a3dd98e4b5ea3cfb5b63ae66732",
-      "creation_time": "3-4-2021 20:21:9",
-      "cpu": 1000,
-      "gpu": 1,
-      "memory": "0.001"
-    }
-  ]
-
-  const appstoreResponse = {
-    "blackbalsam": {
-      "name": "Blackbalsam",
-      "app_id": "blackbalsam",
-      "description": "An A.I., visualization, and parallel computing environment.",
-      "detail": "A.I.(Tensorflow,Keras,PyTorch,Gensim) Vis(Plotly,Bokeh,Seaborn) Compute(Spark).",
-      "docs": "https://github.com/stevencox/blackbalsam",
-      "spec": "https://github.com/helxplatform/app-support-prototype/raw/master/dockstore-yaml-proposals/blackbalsam/docker-compose.yaml",
-      "cpu": 0,
-      "gpu": 0,
-      "memory": "1000M"
-    },
-    "cloud-top": {
-      "name": "Cloud Top",
-      "app_id": "cloud-top",
-      "description": "CloudTop is a cloud native, browser accessible Linux desktop.",
-      "detail": "A Ubuntu graphical desktop environment for experimenting with native applications in the cloud.",
-      "docs": "https://helxplatform.github.io/cloudtop-docs/",
-      "spec": "https://github.com/helxplatform/app-support-prototype/raw/master/dockstore-yaml-proposals/cloud-top/docker-compose.yaml",
-      "cpu": 0,
-      "gpu": 0,
-      "memory": "1000M"
-    },
-    "imagej": {
-      "name": "ImageJ Viewer",
-      "app_id": "imagej",
-      "description": "Imagej is an image processor developed at NIH/LOCI.",
-      "detail": "can display, edit, analyze, process, save and print 8-bit, 16-bit and 32-bit images. It can read many image formats.",
-      "docs": "https://imagej.nih.gov/ij/",
-      "spec": "https://github.com/helxplatform/app-support-prototype/raw/master/dockstore-yaml-proposals/imagej/docker-compose.yaml",
-      "cpu": 0,
-      "gpu": 0,
-      "memory": "2000M"
-    },
-    "jupyter-ds": {
-      "name": "Jupyter Data Science",
-      "app_id": "jupyter-ds",
-      "description": "Jupyter DataScience - A Jupyter notebook for exploring and visualizing data.",
-      "detail": "Includes R, Julia, and Python.",
-      "docs": "https://jupyter-docker-stacks.readthedocs.io/en/latest/using/selecting.html#jupyter-datascience-notebook",
-      "spec": "https://github.com/helxplatform/app-support-prototype/raw/master/dockstore-yaml-proposals/jupyter-ds/docker-compose.yaml",
-      "cpu": 0,
-      "gpu": 0,
-      "memory": "1000M"
-    },
-    "napari": {
-      "name": "Napari Image Viewer",
-      "app_id": "napari",
-      "description": "Napari is a fast, interactive, multi-dimensional image viewer.",
-      "detail": "It enables browsing, annotating, and analyzing large multi-dimensional images.",
-      "docs": "https://napari.org/",
-      "spec": "https://github.com/helxplatform/app-support-prototype/raw/master/dockstore-yaml-proposals/napari/docker-compose.yaml",
-      "cpu": 0,
-      "gpu": 0,
-      "memory": "8000M"
-    }
-  }
-
   useEffect(async () => {
-    // const response = await axios({
-    //   url: `${helxAppstoreUrl}/api/v1/apps`,
-    //   method: 'GET',
-    //   headers: {
-    //     'X-CSRFToken': helxAppstoreCsrfToken
-    //   }
-    // }).then((res) => {
-    //   console.log(res);
-
-    // }).catch((e) => {
-    //   console.log(e);
-    // })
-    setApps(appstoreResponse);
     if (active === 'Apps') {
-      setApps(appstoreResponse);
+
+      // hit /apps endpoint and store apps info
+      const response = await axios({
+        url: `${helxAppstoreUrl}/apps`,
+        method: 'GET',
+        headers: {
+          'X-CSRFToken': helxAppstoreCsrfToken
+        }
+      }).then((res) => {
+        setApps(res);
+      }).catch((e) => {
+        console.log(e);
+      })
       setServices([]);
     }
     else {
       setApps({});
-      setServices(serviceResponse);
+
+      // hit /services endpoint and store all running apps
+      const response = await axios({
+        url: `${helxAppstoreUrl}/services`,
+        method: 'GET',
+        headers: {
+          'X-CSRFToken': helxAppstoreCsrfToken
+        }
+      }).then((res) => {
+        setServices(res);
+      }).catch((e) => {
+        console.log(e);
+      })
     }
   }, [active])
 
