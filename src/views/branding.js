68cd87e958c26874440659ec9deef06e2d7f0a12
Alexander Hagerman <ahagerman@renci.org>
2021-03-31 09:10:19 -0400

    Update branding
    - Remove lorem
    - Add HeLx about

--- src/views/branding.js
+++ src/views/branding.js
@@ -15,15 +15,6 @@ export const Branding = () => {
   return (
     <Container>
       <Title>HeLx Branding</Title>
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </Paragraph>
-
       <Section>
         <Heading>Buttons</Heading>
 
@@ -50,7 +41,6 @@ export const Branding = () => {
         <Button small variant="warning">Warning</Button> &nbsp;
         <Button small variant="info">Info</Button> &nbsp;
         <Button small variant="success">Success</Button> &nbsp;
-
       </Section>
 
       <Section>
--- src/views/home.js
+++ src/views/home.js
@@ -7,29 +7,16 @@ export const Home = () => {
   const { addNotification } = useNotifications()
   return (
     <Container>
-      <Title>Home</Title>
+      <Title>HeLx</Title>
 
       <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </Paragraph>
-
-    <Heading>Add Notifications</Heading>
-    
-    <button onClick={() => addNotification({ type: 'info', text: 'here is some information' })}>info notification</button>
-    <br /><br />
-    <button onClick={() => addNotification({ type: 'info', text: 'here is some information. you must close me.', autoClose: false })}>stubborn info notification</button>
-    <br /><br />
-    <button onClick={() => addNotification({ type: 'error', text: 'an error occurred' })}>error notification</button>
-    <br /><br />
-    <button onClick={() => addNotification({ type: 'success', text: 'that was successful' })}>success notification</button>
-    <br /><br />
-    <button onClick={() => addNotification({ type: 'warning', text: 'be careful' })}>warning notification</button>
-    <br /><br />
+        The HeLx AppStore is the primary user experience component of the HeLx
+        data science platform. Through the AppStore, users discover and interact
+        with analytic tools and data to explore scientific problems. Its ability
+        to empower researchers to leverage advanced analytical tools without
+        installation or other infrastructure concerns has broad reaching benefits
+        and can be applied in many domains.
+      </Paragraph>
   </Container>
   )
 }
