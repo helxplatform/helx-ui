2a08329df263ae36ee6ed7e18297cd40d0873cb2
connectbo <zbo@renci.org>
2021-04-06 10:53:54 -0400

    style homepage

--- src/views/home.js
+++ src/views/home.js
@@ -1,22 +1,38 @@
 import React, { useState } from 'react'
+import styled, { useTheme } from 'styled-components'
 import { Container } from '../components/layout'
 import { Title, Heading, Paragraph } from '../components/typography'
-import { useNotifications } from '../components/notifications'
+import HeLxLogo from '../images/helx-logo-blue.svg';
+
+const HomeContainer = styled(Container)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+`
+const HeLxImage = styled.img`
+  width: 30vw;
+`
+
+const HeLxIntro = styled.div`
+  width: 40vw;
+`
 
 export const Home = () => {
-  const { addNotification } = useNotifications()
   return (
     <Container>
       <Title>HeLx</Title>
-
-      <Paragraph>
-        The HeLx AppStore is the primary user experience component of the HeLx
-        data science platform. Through the AppStore, users discover and interact
-        with analytic tools and data to explore scientific problems. Its ability
-        to empower researchers to leverage advanced analytical tools without
-        installation or other infrastructure concerns has broad reaching benefits
-        and can be applied in many domains.
-      </Paragraph>
-  </Container>
+      <HomeContainer>
+        <HeLxIntro>
+          The HeLx AppStore is the primary user experience component of the HeLx
+          data science platform. Through the AppStore, users discover and interact
+          with analytic tools and data to explore scientific problems. Its ability
+          to empower researchers to leverage advanced analytical tools without
+          installation or other infrastructure concerns has broad reaching benefits
+          and can be applied in many domains.
+      </HeLxIntro>
+        <HeLxImage src={HeLxLogo} />
+      </HomeContainer>
+    </Container>
   )
 }
