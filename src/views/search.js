a565160d21ed140eb7ecec524f93e50e58b80581
connectbo <zbo@renci.org>
2021-04-06 10:51:31 -0400

    remove old auth logic

--- src/views/search.js
+++ src/views/search.js
@@ -3,14 +3,10 @@ import { Container } from '../components/layout'
 import { Title } from '../components/typography'
 import { HelxSearch, SearchForm, SearchResults } from '../components/search'
 import { Link } from '../components/link'
-import { Whitelist } from '../views/whitelist';
-import { useEnvironment } from '../contexts/';
+import { useAuth } from '../contexts/';
 
 export const Search = () => {
-  const { isAuth } = useEnvironment();
-
-  if (!isAuth) return <Whitelist />
-
+  
   return (
     <Container>
       <Title>Search</Title>
