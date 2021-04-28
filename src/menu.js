e11c2cec1eef342036be1a9ae6c7880ea34fb388
connectbo <zbo@renci.org>
2021-04-28 13:21:33 -0400

    remove about from menu

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,4 @@
 export const menuItems = [
-  { text: 'About', path: '/frontend/react/about', requiresAuth: false },
   { text: 'Workspaces', path: '/frontend/react/workspaces', requiresAuth: false },
   { text: 'Docs', path: 'https://helx.gitbook.io/helx-documentation/', requiresAuth: false },
   { text: 'Contact', path: 'mailto:helx@lists.renci.org', requiresAuth: false },
