e2fb6f18ec7def946ac8cc148d5a40e267db2417
connectbo <zbo@renci.org>
2021-05-06 13:58:56 -0400

    refactor selected menu-items

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import styled, { useTheme } from 'styled-components'
 import { Link } from '../link'
+import { Button } from '../button'
 import { useAuth } from '../../contexts'
 import { Icon } from '../icon'
 
@@ -13,35 +14,57 @@ const Wrapper = styled.nav`
 `
 
 const MenuItem = styled(Link)(({ theme }) => `
-  color: ${ theme.color.black };
+  color: ${theme.color.black};
   text-decoration: none;
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100%;
-  padding: ${ theme.spacing.medium };
+  padding: ${theme.spacing.medium};
   text-transform: uppercase;
   border-width: 2px 0 2px 0;
   border-style: solid;
   border-color: transparent;
   transition: background-color 250ms, color 250ms, border-color 250ms;
-  &:hover, &:focus {
-    background-color: ${ theme.color.grey.light };
+  &:hover, :focus{
+    background-color: ${theme.color.grey.light};
   }
-  &[aria-current] {
-    background-color: ${ theme.color.grey.light };
-    border-bottom-color: ${ theme.color.grey.dark };
+  &.selected{
+  background-color: ${theme.color.grey.light};
+  border-bottom-color: ${theme.color.grey.dark};
   }
 `)
 
 export const Menu = ({ items }) => {
   const theme = useTheme()
   const auth = useAuth()
+
+  // store the last selected menu item
+  const [prevMenu, setPrevMenu] = useState();
+
+  const menuHandler = (prop) => {
+    // unselect the previous menu item
+    if (prevMenu !== undefined) {
+      let prevMenuItem = document.getElementById(`menu-${prevMenu}`);
+      prevMenuItem.classList.remove('selected');
+    }
+    // select the new menu item
+    let currMenu = document.getElementById(`menu-${prop}`);
+    currMenu.classList.add('selected');
+    setPrevMenu(prop);
+  }
+
+  // set active menu area on each render
+  useEffect(() => {
+    const currPath = window.location.pathname.split('/')[2];
+    menuHandler(currPath);
+  }, [])
+
   return (
     <Wrapper>
-      { items.map(item => <MenuItem to={ item.path } key={ item.text }>{ item.text }</MenuItem>) }
-      <MenuItem to="/helx/account">
-        { auth.user ? <Icon icon="userCircle" fill={ theme.color.grey.main } size={ 24 }/> : 'LOGIN' }
+      { items.map(item => <MenuItem id={`menu-${item.text}`} to={item.path} key={item.text} onClick={() => menuHandler(item.text)}>{item.text}</MenuItem>)}
+      <MenuItem id={'menu-account'} to="/helx/account" onClick={() => menuHandler('account')}>
+        {auth.user ? <Icon icon="userCircle" fill={theme.color.grey.main} size={24} /> : 'LOGIN'}
       </MenuItem>
     </Wrapper>
   )
@@ -49,7 +72,7 @@ export const Menu = ({ items }) => {
 
 Menu.propTypes = {
   items: PropTypes.arrayOf(PropTypes.shape({
-     text: PropTypes.string.isRequired,
-     path: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    path: PropTypes.string.isRequired,
   })).isRequired,
 }
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,5 @@
 export const menuItems = [
-  { text: 'Workspaces', path: '/helx/workspaces', requiresAuth: false },
-  { text: 'Docs', path: 'https://helx.gitbook.io/helx-documentation/', requiresAuth: false },
-  { text: 'Contact', path: 'mailto:helx@lists.renci.org', requiresAuth: false },
+  { text: 'workspaces', path: '/helx/workspaces', requiresAuth: false },
+  { text: 'docs', path: 'https://helx.gitbook.io/helx-documentation/', requiresAuth: false },
+  { text: 'contact', path: 'mailto:helx@lists.renci.org', requiresAuth: false },
 ]
