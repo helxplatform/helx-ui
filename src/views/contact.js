b796dc0460002d6c9cd447274a0391b186ddca1b
connectbo <zbo@renci.org>
2021-05-11 14:53:16 -0400

    remove mailto

--- src/views/contact.js
+++ src/views/contact.js
@@ -6,7 +6,7 @@ export const Contact = () => {
     return (
         <Container>
             <Heading>Get in touch</Heading>
-            <div>For support or any help, please reach out to <a href="mailto:helx@lists.renci.org">helx@lists.renci.org.</a></div>
+            <div>For support or any help, please reach out to helx@lists.renci.org.</div>
         </Container>
     )
 }
