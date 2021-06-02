f42852ca441da2d959a9c689b4ddb4e3a6d47d3c
connectbo <zbo@renci.org>
2021-06-02 18:11:41 -0400

    fix active menu item

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { Router } from '@reach/router'
+import { LocationProvider, Router } from '@reach/router'
 import { AppProvider, EnvironmentProvider, InstanceProvider } from './contexts'
 import {
   ActiveView,
@@ -12,21 +12,23 @@ import { Layout } from './components/layout'
 export const App = () => {
   return (
     <EnvironmentProvider>
-      <AppProvider>
-        <InstanceProvider>
-          <Layout>
-            <Router basepath="/helx">
-              <AvailableView path="/workspaces" />
-              <AvailableView path="/workspaces/available" />
-              <ActiveView path="/workspaces/active" />
-              <SupportView path="/support" />
-              <SearchView path="/" />
-              <SearchView path="/search" />
-              <NotFoundView default />
-            </Router>
-          </Layout>
-        </InstanceProvider>
-      </AppProvider>
+      <LocationProvider>
+        <AppProvider>
+          <InstanceProvider>
+            <Layout>
+              <Router basepath="/helx">
+                <AvailableView path="/workspaces" />
+                <AvailableView path="/workspaces/available" />
+                <ActiveView path="/workspaces/active" />
+                <SupportView path="/support" />
+                <SearchView path="/" />
+                <SearchView path="/search" />
+                <NotFoundView default />
+              </Router>
+            </Layout>
+          </InstanceProvider>
+        </AppProvider>
+      </LocationProvider>
     </EnvironmentProvider >
   )
 }
