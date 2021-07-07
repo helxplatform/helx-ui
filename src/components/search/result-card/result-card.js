e9490b6aa803331b517c21fe4660d9a902f57403
Matt Watson <mattwat@gmail.com>
2021-07-06 23:52:37 -0400

    separate components into directories

--- src/components/search/result-card/result-card.js
+++ src/components/search/result-card/result-card.js
@@ -1,10 +1,9 @@
 import { Fragment, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { useHelxSearch } from './context'
+import { Link } from '../../link'
 import { Card, List, Space, Tag, Typography } from 'antd'
-import { Link } from '../link'
 import { ExpandOutlined as ViewIcon } from '@ant-design/icons'
-import { KnowledgeGraphs } from './'
+import { KnowledgeGraphs, useHelxSearch } from '../'
 import './result-card.css'
 
 const { Meta } = Card
