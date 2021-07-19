8c893f267931b73153d6a4bc8ea44dc8a74b6d00
connectbo <zbo@renci.org>
2021-07-19 17:54:32 -0400

    refactor app architecture

--- src/app.js
+++ src/app.js
@@ -1,65 +1,12 @@
-import { Fragment } from 'react'
-import { LocationProvider, Router } from '@reach/router'
-import { ActivityProvider, AppProvider, EnvironmentProvider, InstanceProvider } from './contexts'
-import {
-  ActiveView,
-  AvailableView,
-  SupportView,
-  NotFoundView,
-  SearchView,
-} from './views'
-import { Layout } from './components/layout'
+import { EnvironmentProvider } from './contexts'
+import { AppWrapper } from './app-wrapper'
 
-const renderSearchModule = () => {
-  if (process.env.REACT_APP_SEMANTIC_SEARCH_ENABLED === 'true') {
-    return <SearchView path="/search" />
-  }
-}
-
-const renderWorkspacesModule = () => {
-  if (process.env.REACT_APP_WORKSPACES_ENABLED === 'true') {
-    return <Fragment>
-      <AvailableView path="/workspaces" />
-      <AvailableView path="/workspaces/available" />
-      <ActiveView path="/workspaces/active" />
-    </Fragment>
-  }
-}
-
-const routeHomepage = () => {
-  if (process.env.REACT_APP_SEMANTIC_SEARCH_ENABLED === 'false') {
-    if (process.env.REACT_APP_WORKSPACES_ENABLED === 'true') {
-      return <AvailableView path="/" />
-    }
-    else {
-      return <SupportView path="/" />
-    }
-  }
-  else {
-    return <SearchView path="/" />
-  }
-}
 
 export const App = () => {
+
   return (
     <EnvironmentProvider>
-      <LocationProvider>
-        <ActivityProvider>
-          <AppProvider>
-            <InstanceProvider>
-              <Layout>
-                <Router basepath="/helx">
-                  <SupportView path="/support" />
-                  {renderSearchModule()}
-                  {renderWorkspacesModule()}
-                  {routeHomepage()}
-                  <NotFoundView default />
-                </Router>
-              </Layout>
-            </InstanceProvider>
-          </AppProvider>
-        </ActivityProvider>
-      </LocationProvider>
+      <AppWrapper />
     </EnvironmentProvider >
   )
 }
