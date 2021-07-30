567e5feb6c75caca4c286540f37970024a37b005
connectbo <zbo@renci.org>
2021-07-30 17:04:45 -0400

    hide search breadcrumb and title when workspace is disabled

--- src/views/search.js
+++ src/views/search.js
@@ -2,10 +2,12 @@ import { Fragment } from 'react'
 import { HelxSearch, SearchForm, SearchResults } from '../components/search'
 import { Typography } from 'antd'
 import { Breadcrumbs } from '../components/layout'
+import { useEnvironment } from '../contexts'
 
 const { Title } = Typography
 
 export const SearchView = () => {
+  const { context } = useEnvironment();
 
   const breadcrumbs = [
     { text: 'Home', path: '/helx' },
@@ -14,11 +16,11 @@ export const SearchView = () => {
 
   return (
     <Fragment>
-      
-      <Breadcrumbs crumbs={ breadcrumbs } />
 
-      <Title level={ 1 }>Search</Title>
-      
+      {context.env.REACT_APP_WORKSPACES_ENABLED === 'true' && <Breadcrumbs crumbs={breadcrumbs} />}
+
+      {context.env.REACT_APP_WORKSPACES_ENABLED === 'true' && <Title level={1}>Search</Title>}
+
       <HelxSearch>
         <SearchForm />
         <SearchResults />
