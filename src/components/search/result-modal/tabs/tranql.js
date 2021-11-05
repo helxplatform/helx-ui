45f2124379ceea04a2601f9c47c74d58814c8412
Griffin Roupe <groupe@rciadmin.wireless-1x.unc.edu>
2021-11-04 23:49:27 -0400

    Adds link to view in full TranQL. Minor changes to TranQL embedding styles. Adds gamma-viewer-web dependency.

--- src/components/search/result-modal/tabs/tranql.js
+++ src/components/search/result-modal/tabs/tranql.js
@@ -1,12 +1,16 @@
-import { Fragment, useCallback, useMemo, useState, useRef } from 'react'
+import React, { Fragment, useCallback, useMemo, useState, useRef } from 'react'
+import ReactDOM from 'react-dom'
 import axios from 'axios'
 import { Button, Divider, Input, Spin, Typography } from 'antd'
 import { useHelxSearch } from '../../'
 import { Link } from '../../../link'
 import { RocketOutlined as QueryIcon } from '@ant-design/icons'
+import GammaViewer from 'gamma-viewer-web'
 import { SizeMe } from 'react-sizeme'
 import { useDebounce } from 'use-debounce'
 import './tranql.css'
+
+// const response = require("../../../../response.json");
 
 axios.defaults.timeout = 30000
 
@@ -43,7 +47,7 @@ export const TranQLTab = ({ result, graphs }) => {
 
   const initialTranqlQuery = useMemo(makeTranqlQuery, [result, graphs])
   const [tranqlQuery, setTranqlQuery] = useState(initialTranqlQuery)
-  const [debouncedQuery] = useDebounce(tranqlQuery, 250)
+  // const [debouncedQuery] = useDebounce(tranqlQuery, 250)
 
   const [iframeLoading, setIframeLoading] = useState(true)
 
@@ -62,24 +66,30 @@ export const TranQLTab = ({ result, graphs }) => {
       {/* <br /> */}
       
       {/* <Divider /> */}
+      {/* <GammaViewer data={response}/> */}
       {
-        debouncedQuery === undefined ? (
-          <>
+        tranqlQuery === undefined ? (
+          <Fragment>
           <Title level={ 4 }>TranQL</Title>
           <Text>No query could be constructed from the result.</Text>
-          </>
+          </Fragment>
         ) : (
+          <Fragment>
           <SizeMe>
             {
               ({ size }) => (
-                <iframe src={`${tranqlUrl}?embed=FULL&q=${encodeURIComponent(debouncedQuery)}`}
-                        height="665" width={size.width}
+                <iframe src={`${tranqlUrl}?embed=FULL&q=${encodeURIComponent(tranqlQuery)}`}
+                        height="500" width={size.width}
                         ref={tranqlIframe}
                         onLoad={() => setIframeLoading(false)}
+                        style={{borderWidth: "0"}}
                 />
               )
             }
           </SizeMe>
+          <br /><br />
+          <Link to={`${tranqlUrl}?q=${encodeURIComponent(tranqlQuery)}`}>View in TranQL</Link>
+          </Fragment>
         )
       }
     </Fragment>
