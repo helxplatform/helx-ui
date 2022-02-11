bd5cbf319cbdecf975bdb6371182a4af6ac1b617
Matt Watson <mwatson@renci.org>
2022-02-11 11:38:40 -0500

    create shopping cart view

---
+++ src/views/cart.js
@@ -0,0 +1,12 @@
+import { Fragment } from 'react'
+import { Typography } from 'antd'
+
+const { Title } = Typography
+
+export const ShoppingCartView = () => {
+  return (
+    <Fragment>
+      <Title level={ 1 }>Shopping Cart</Title>
+    </Fragment>
+  )
+}
