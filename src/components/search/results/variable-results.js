d61470b79a18f5be7f6114cacc8b4af5e925f0c7
connectbo <zbo@renci.org>
2022-02-26 19:45:07 -0500

    refactor antv components and add highlight function

--- src/components/search/results/variable-results.js
+++ src/components/search/results/variable-results.js
@@ -1,6 +1,8 @@
-import React, { useEffect, memo, useState } from 'react'
-import { Column } from '@antv/g2plot';
+import React, { useEffect, memo, useState, useRef } from 'react'
+import { Column } from '@ant-design/plots';
 import { useHelxSearch } from '..';
+import { Button } from 'antd';
+import './variable-results.css';
 
 const paletteSemanticRed = '#F4664A';
 const brandColor = '#5B8FF9';
@@ -9,7 +11,8 @@ export const VariableSearchResults = () => {
     const { variableResults } = useHelxSearch()
     const [filteredVariables, setFilteredVariables] = useState(variableResults)
     const [highlightStudies, setHighlightStudies] = useState('')
-    const [highlightVariables, setHighLightVariables] = useState([])
+    const [highlightVariables, setHighLightVariables] = useState(variableResults)
+    const histogram = useRef()
 
     // initial config for the histogram
     const initialConfig = {
@@ -27,31 +30,41 @@ export const VariableSearchResults = () => {
         }
     }
 
-    useEffect(() => {
-            const column = new Column('histogram-container', initialConfig);
-            console.log(column)
-            column.render()
-            // event handler for slider, so 'filteredVariables' state will be the variables shown on the histogram
-            column.on('slider:mouseup', (e) => {
-                setFilteredVariables(e.view.filteredData)
-            })
+    const startOverHandler = () => {
+        setHighLightVariables(variableResults)
+        setHighlightStudies('')
+    }
 
-            column.on('plot:click', (e) => {
-                if (e?.data?.data) {
-                    const variablesPerStudy = variableResults.filter(variable => variable.study_name === e.data.data.study_name)
-                    setHighlightStudies(e.data.data.study_name)
-                    setHighLightVariables(variablesPerStudy)
-                    column.update({ ...initialConfig, data: variablesPerStudy })
-                }
-            })
+    useEffect(() => {
+        let histogramObj = histogram.current.getChart()
+        histogramObj.on('slider:mouseup', (e) => {
+            setFilteredVariables(e.view.filteredData)
+        })
+        histogramObj.on('plot:click', (e) => {
+            if (e?.data?.data) {
+                const newHighLightVariables = variableResults.filter(variable => variable.study_name === e.data.data.study_name)
+                const newHighLightStudy = e.data.data.study_name
+                setHighLightVariables(newHighLightVariables)
+                setHighlightStudies(newHighLightStudy)
+            }
+        })
     }, [])
+
+    useEffect(() => {
+        let histogramObj = histogram.current.getChart()
+        histogramObj.update({ ...initialConfig, data: highlightVariables })
+    }, [highlightStudies])
 
     return (
         <div>
-            <button>Start Over</button>
-            <div id="histogram-container"></div>
-            <div>Study: {highlightStudies ? highlightStudies : 'Not Selected'} </div>
-            <div>{highlightVariables.length} variables</div>
+            <Button className="histogram-startover-btn" onClick={startOverHandler}>Start Over</Button>
+            <Column
+                {...initialConfig}
+                ref={histogram}
+            />
+            <div>{filteredVariables.length}</div>
+            <div>Highlighted Study: {highlightStudies ? highlightStudies : 'Not Selected'} </div>
+            <div>{highlightVariables.length} highlighted variables</div>
         </div>
     )
 }
