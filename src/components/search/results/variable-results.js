59bb237ae147c6a3039aed09050220d4e636327c
Ginnie Hench <vhench@rti.org>
2022-03-02 15:31:16 -0500

    Get brush effect working
    - Had to remove on-click highlight functions
    - Set up tooltip properties, update x-axis
    - Remove some unused vars & methods

--- src/components/search/results/variable-results.js
+++ src/components/search/results/variable-results.js
@@ -1,38 +1,34 @@
-import React, { useEffect, memo, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { Column } from '@ant-design/plots';
 import { useHelxSearch } from '..';
 import { Button } from 'antd';
 import './variable-results.css';
 
-const paletteSemanticRed = '#F4664A';
-const brandColor = '#5B8FF9';
-
 export const VariableSearchResults = () => {
     const { variableResults } = useHelxSearch()
     const [filteredVariables, setFilteredVariables] = useState(variableResults)
-    const [highlightStudies, setHighlightStudies] = useState('')
-    const [highlightVariables, setHighLightVariables] = useState(variableResults)
     const histogram = useRef()
 
-    // initial config for the histogram
-    const initialConfig = {
-        data: variableResults,
+    // initial config for the variables histogram
+    const variableHistogramConfig = {
+        data: filteredVariables,
         xField: 'id',
         yField: 'score',
         xAxis: {
-            label: {
-                autoRotate: false,
-            },
+            label: ""
         },
         brush: {
             enabled: true,
             type: 'x-rect',
+        },
+        tooltip: {
+            showTitle: false,
+            fields: ['name', 'description', 'study_name', 'score','index_pos'],
         }
     }
 
     const startOverHandler = () => {
-        setHighLightVariables(variableResults)
-        setHighlightStudies('')
+        setFilteredVariables(variableResults)
     }
 
     useEffect(() => {
@@ -42,33 +38,21 @@ export const VariableSearchResults = () => {
             console.log(e.view.filteredData)
             setFilteredVariables(e.view.filteredData)
         })
-
-        // The below functionality isn't needed now (3/1/22), but is still useful to have worked out
-        histogramObj.on('plot:click', (e) => {
-            if (e?.data?.data) {
-                const newHighLightVariables = variableResults.filter(variable => variable.study_name === e.data.data.study_name)
-                const newHighLightStudy = e.data.data.study_name
-                setHighLightVariables(newHighLightVariables)
-                setHighlightStudies(newHighLightStudy)
-            }
-        })
     }, [])
 
     useEffect(() => {
         let histogramObj = histogram.current.getChart()
-        histogramObj.update({ ...initialConfig, data: highlightVariables })
-    }, [highlightStudies])
+        histogramObj.update({ ...variableHistogramConfig, data: filteredVariables })
+    }, [filteredVariables])
 
     return (
         <div>
             <Button className="histogram-startover-btn" onClick={startOverHandler}>Start Over</Button>
             <Column
-                {...initialConfig}
+                {...variableHistogramConfig}
                 ref={histogram}
             />
-            <div>{filteredVariables.length}</div>
-            <div>Highlighted Study: {highlightStudies ? highlightStudies : 'Not Selected'} </div>
-            <div>{highlightVariables.length} highlighted variables</div>
+            <div>Filtered Variables Count: {filteredVariables.length}</div>
         </div>
     )
 }
