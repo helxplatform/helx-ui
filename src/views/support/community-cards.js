5a20decc9995dc52a0124c1becff00460fc30bf0
Griffin Roupe <groupe@renci.org>
2022-05-25 16:33:50 -0400

    Add functionality to support card buttons. Fix community support text.

--- src/views/support/community-cards.js
+++ src/views/support/community-cards.js
@@ -9,16 +9,16 @@ export const CommunitySupport = () => (
     hoverable
     className="community-support-card"
     actions={[
-        <Button icon={ <ExportOutlined /> }>Open</Button>
+        <Button icon={ <ExportOutlined /> } href="https://community.helx.renci.org/invites/B2dnCA8xRi" target="_blank" rel="noopener noreferrer">Open</Button>
     ]}
     >
         <img src="http://www.cniins.com/files/2018/01/community-image.gif"/>
         <Card.Meta
             title={
-            <Title level={3} style={{ whiteSpace: "normal", marginBottom: "8px" }}>Community Support</Title>
+                <Title level={3} style={{ whiteSpace: "normal", marginBottom: "8px" }}>Community Support</Title>
             }
             description={
-            <Text>Familiarize yourself with Dug, our semantic search tool.</Text>
+                <Text>Need help? Ask questions using your community's Discourse forum.</Text>
             }
         />
     </Card>
--- src/views/support/documentation-cards.js
+++ src/views/support/documentation-cards.js
@@ -1,5 +1,5 @@
 import { Button, Card, Typography } from "antd";
-import { ExportOutlined } from "@ant-design/icons"
+import { ExportOutlined, RocketOutlined, ControlOutlined, UnorderedListOutlined } from "@ant-design/icons"
 
 const { Title, Text } = Typography
 
@@ -9,7 +9,7 @@ export const UsingDug = () => (
       hoverable
       className="using-dug-card"
       actions={[
-        <Button icon={ <ExportOutlined /> }>Open</Button>
+        <Button icon={ <ExportOutlined /> } href="https://helx.gitbook.io/helx-documentation/dug/" target="_blank" rel="noopener noreferrer">Open</Button>
       ]}
     >
       <img src="https://www.pinclipart.com/picdir/big/535-5351710_european-robin-transparent-background-clipart.png"/>
@@ -29,7 +29,15 @@ export const GettingStarted = () => (
       hoverable
       className="getting-started-card"
       actions={[
-        <Button icon={ <ExportOutlined /> }>Open</Button>
+        <Button icon={ <RocketOutlined /> } href="https://helx.gitbook.io/helx-documentation/helx/starting-an-existing-app" target="_blank" rel="noopener noreferrer">
+          Launch
+        </Button>,
+        <Button icon={ <ControlOutlined /> } href="https://helx.gitbook.io/helx-documentation/helx/application-management" target="_blank" rel="noopener noreferrer">
+          Manage
+        </Button>,
+        <Button icon={ <UnorderedListOutlined /> } href="https://helx.gitbook.io/helx-documentation/helx/helx-workspaces" target="_blank" rel="noopener noreferrer">
+          Apps
+        </Button>
       ]}
     >
       <img src="/static/frontend/kindpng_2545457-removebg-preview.png"/>
@@ -50,7 +58,12 @@ export const TechnicalDocs = () => (
       hoverable
       className="technical-docs-card"
       actions={[
-        <Button icon={ <ExportOutlined /> }>Open</Button>
+        <Button icon={ <ExportOutlined /> } href="https://helx.gitbook.io/helx-documentation/dug/technical-documents" target="_blank" rel="noopener noreferrer">
+          Dug
+        </Button>,
+        <Button icon={ <ExportOutlined /> } href="https://helx.gitbook.io/helx-documentation/helx/technical-documents" target="_blank" rel="noopener noreferrer">
+          Workspaces
+        </Button>
       ]}
       // style={{ marginRight: "16px" }}
     >
