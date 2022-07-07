2b635729eff39b8243306a728a073d049c5b3dce
Griffin Roupe <groupe@renci.org>
2022-07-07 14:40:15 -0400

    Add facet filtering back to card studies tab & improves styling slightly.

--- src/components/search/concept-card/studies-tab.js
+++ src/components/search/concept-card/studies-tab.js
@@ -1,19 +1,38 @@
-import { Fragment, useEffect, useMemo, useState } from 'react'
-import { List, Spin, Space, Tag, Typography } from 'antd'
+import { Fragment, useCallback, useEffect, useMemo, useState } from 'react'
+import { List, Spin, Space, Tag, Typography, Divider } from 'antd'
 import { useHelxSearch } from '../'
 import { Link } from '../../link'
 
 const { Text } = Typography
+const { CheckableTag: CheckableFacet } = Tag
 
 export const StudiesTab = ({ result }) => {
   const { query, fetchStudyVariables, fetchCDEs } = useHelxSearch()
   const [studies, setStudies] = useState([])
   const [loading, setLoading] = useState(true)
+  const [facets, setFacets] = useState([])
+  const [selectedFacets, setSelectedFacets] = useState([])
 
-  const facets = useMemo(() => studies.reduce((acc, study) => {
-    if (!acc.includes(study.type)) acc.push(study.type)
-    return acc
-  }, []), [studies])
+  const studiesSource = useMemo(() => studies.filter((study) => selectedFacets.includes(study.type)), [studies, selectedFacets])
+
+  const handleSelectFacet = useCallback((facet, checked) => {
+    const newSelection = new Set(selectedFacets)
+    if (newSelection.has(facet)) {
+      newSelection.delete(facet)
+    } else {
+      newSelection.add(facet)
+    }
+    setSelectedFacets([...newSelection])
+  }, [selectedFacets])
+
+  useEffect(() => {
+    const facets = studies.reduce((acc, study) => {
+      if (!acc.includes(study.type)) acc.push(study.type)
+      return acc
+    }, [])
+    setFacets(facets)
+    setSelectedFacets(facets)
+  }, [studies])
   
   useEffect(() => {
     const getStudies = async () => {
@@ -26,7 +45,6 @@ export const StudiesTab = ({ result }) => {
         elements: null,
         type: "CDE"
       })) : []
-      console.log(cdes)
       setStudies([
         ...studies,
         ...cdesAsStudies
@@ -43,21 +61,25 @@ export const StudiesTab = ({ result }) => {
   
   return (
     <Space direction="vertical" className="tab-content">
-      {/* <Space direction="horizontal" size="small">
+      <Space direction="horizontal" size="small">
+        {/* <Text style={{ marginRight: "4px" }}>Studies:</Text> */}
         {
-          facets.map(facet => studies[facet] && (
+          facets.map((facet) => (
             <CheckableFacet
               key={ `search-facet-${ facet }` }
+              style={ !selectedFacets.includes(facet) ? { border: "1px solid #d9d9d9", background: "#fafafa" } : {} }
               checked={ selectedFacets.includes(facet) }
-              onChange={ checked => handleSelectFacet(facet, checked) }
-              children={ `${ facet } (${studies[facet].length})` }
+              onChange={ (checked) => handleSelectFacet(facet, checked) }
+              children={ `${ facet } (${studies.filter((study) => study.type === facet).length})` }
             />
           ))
         }
-      </Space> */}
+      </Space>
+      {/* <Divider style={{ marginTop: "8px", marginBottom: "8px" }} /> */}
       <List
         className="studies-list"
-        dataSource={ studies }
+        locale={{ emptyText: "" }}
+        dataSource={ studiesSource }
         renderItem={ study => (
           <List.Item className="studies-list-item">
             <Tag className="study-tag">{ study.type }</Tag>
