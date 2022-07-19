163b11df054c20b55c4c401941df94a26806395b
Griffin Roupe <groupe@renci.org>
2022-07-19 18:16:42 -0400

    Adds basic functionality to shopping cart popup + context tweaks

--- src/components/shopping-cart/shopping-cart-popover.js
+++ src/components/shopping-cart/shopping-cart-popover.js
@@ -1,29 +1,158 @@
-import { Badge, Button, Popover, Space, List, Typography } from 'antd'
+import { Fragment, useEffect, useState } from 'react'
+import { Badge, Button, Popover, Space, List, Typography, Input } from 'antd'
+import { useShoppingCart } from '../../contexts/'
+import './shopping-cart-popover.css'
 
-const { Title, Text } = Typography
+const { Title, Text, Paragraph } = Typography
+
+const ManageCarts = ({ close }) => {
+  const { carts, activeCart, setActiveCart, removeCart } = useShoppingCart()
+
+  const selectCart = (cart) => {
+    setActiveCart(cart.name)
+    close()
+  }
+
+  return (
+    <Fragment>
+      <List
+        className="manage-cart-list"
+        dataSource={ carts }
+        renderItem={(cart) => (
+          <List.Item key={cart.name} className="manage-cart-item">
+            <Text style={{ color: "rgba(0, 0, 0, 0.85)", fontWeight: cart === activeCart ? 600 : 400 }}>
+              {cart.name} ({cart.getCount()})
+            </Text>
+            <Space style={{ flex: 0 }}>
+              <Button type="primary" size="small" onClick={ () => selectCart(cart) }>Use</Button>
+              {/* <Tooltip title="This cart cannot be deleted"> */}
+                <Button type="ghost" size="small" disabled={ !cart.canDelete } onClick={ () => removeCart(cart.name) }>Delete</Button>
+              {/* </Tooltip> */}
+              
+            </Space>
+          </List.Item>
+        )}
+      />
+    </Fragment>
+  )
+}
+
+const CartCreator = ({ cartName, setCartName }) => {
+  return (
+    <Space direction="vertical" size="middle" style={{ marginTop: 8 }}>
+      <Space direction="vertical">
+        Cart name
+        <Input size="small" value={ cartName } onchange={ (e) => setCartName(e.target.value) } />
+      </Space>
+      <Paragraph>
+        Add items to a cart to...
+      </Paragraph>
+    </Space>
+  )
+}
+
+const CartList = () => {
+  return (
+    <List
+    />
+  )
+}
+
+const ShoppingCartPopoverContent = () => {
+  const { carts, activeCart, addCart, setActiveCart } = useShoppingCart()
+  const [manageCarts, setManageCarts] = useState(false)
+  const [creatingCart, setCreatingCart] = useState(false)
+
+  const [cartName, setCartName] = useState("")
+
+  useEffect(() => {
+    if (creatingCart) {
+      const highestExistingDefault = carts
+        .map((cart) => /Shopping Cart (?<num>\d+)/.exec(cart.name)?.groups.num)
+        .filter((match) => match !== undefined)
+        .sort((a, b) => b - a)[0]
+    const defaultName = `Shopping Cart ${highestExistingDefault !== undefined ? parseInt(highestExistingDefault) + 1 : 1}`
+    setCartName(defaultName)}
+  }, [creatingCart])
+
+  const createShoppingCart = () => {
+    addCart(cartName)
+    setCreatingCart(false)
+    setManageCarts(false)
+    setActiveCart(cartName)
+  }
+
+  return (
+    <Space direction="vertical">
+      { creatingCart ? (
+        <div>
+          <Text type="secondary" style={{ fontSize: 15, fontWeight: 500 }}>Create a new cart</Text>
+        </div>
+      ) : manageCarts ? (
+        <div>
+          <Text type="secondary" style={{ fontSize: 15, fontWeight: 500 }}>Manage carts</Text>
+        </div>
+      ) : (
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <Text type="secondary" style={{ fontSize: 15, fontWeight: 500 }}>{ activeCart.name }</Text>
+          <a type="button" onClick={ () => setManageCarts(true) }>View carts</a>
+        </div>
+      )}
+      { creatingCart ? (
+        <CartCreator cartName={ cartName } setCartName={ setCartName } />
+        ) : manageCarts ? (
+        <ManageCarts close={ () => setManageCarts(false) } />
+      ) : (
+        <CartList />
+      )}
+      { creatingCart ? (
+        <Space className="shopping-cart-popover-buttons" style={{ justifyContent: "flex-end" }}>
+          <Button size="small" onClick={ () => setCreatingCart(false) }>Cancel</Button>
+          <Button size="small" type="primary" onClick={ createShoppingCart }>Create cart</Button>
+        </Space>
+      ) : (
+      <Space className="shopping-cart-popover-buttons">
+        <Button onClick={ () => setCreatingCart(true) }>Create a cart</Button>
+        <Button type="primary">
+          Checkout
+        </Button>
+      </Space>
+      )}
+    </Space>
+  )
+}
 
 export const ShoppingCartPopover = ({ visible, onVisibleChange, children }) => {
+  // useEffect(() => {
+  //   if (!visible) 
+  // }, [visible])
   return (
     <Badge count={0} offset={[-8, 0]}>
       <Popover
         title={
-          <Title level={5} type="secondary" style={{ marginTop: 8, marginBottom: 8 }}>Shopping Cart</Title>
+          <Title
+            level={5}
+            style={{
+              marginTop: 8,
+              marginBottom: 8,
+              fontWeight: 600,
+              fontSize: 12,
+              letterSpacing: "0.5px",
+              color: "#434343",
+              textTransform: "uppercase"
+            }}
+          >
+            Shopping Cart
+          </Title>
         }
         content={
-          <Space direction="vertical">
-            <List
-            />
-            <Space style={{ justifyContent: "flex-end" }}>
-              <Button type="primary">
-                Checkout
-              </Button>
-            </Space>
-          </Space>
+          <ShoppingCartPopoverContent />
         }
         placement="bottomLeft"
         trigger="click"
         visible={ visible }
         onVisibleChange={ onVisibleChange }
+        overlayStyle={{ minWidth: 300 }}
       >
         { children }
       </Popover>
--- src/contexts/shopping-cart-context.js
+++ src/contexts/shopping-cart-context.js
@@ -1,46 +1,58 @@
-import { createContext, useCallback, useContext, useEffect, useState } from 'react'
+import { createContext, useCallback, useContext, useEffect, useMemo, useState } from 'react'
 import { useLocalStorage } from '../hooks/use-local-storage'
 
 const ShoppingCartContext = createContext({})
 
+const cartUtilities = {
+  getCount() { return this.concepts.length + this.variables.length + this.studies.length }
+}
 const cartBlueprint = {
   name: undefined,
   createdTime: undefined,
   modifiedTime: undefined,
+  canDelete: true,
   concepts: [],
   variables: [],
   studies: [],
+  ...cartUtilities
 }
 
 export const ShoppingCartProvider = ({ children }) => {
-  const createCart = (name) => {
+  const createCart = (name, props) => {
     return {
       ...cartBlueprint,
+      ...props,
       name,
       createdTime: Date.now(),
       modifiedTime: Date.now()
     }
   }
-  const [carts, setCarts] = useLocalStorage("shoppingCarts", {
-    "default": createCart("Default")
-  })
-  // const [activeCart, setActiveCart] = useState("default")
+  const [carts, setCarts] = useLocalStorage("shopping_carts", [ createCart("My cart", { canDelete: false }) ])
+  const [activeCartName, setActiveCart] = useState("My cart")
+  const activeCart = useMemo(() => carts.find((cart) => cart.name === activeCartName), [carts, activeCartName])
 
-  const addCart = (id, name) => {
-    setCarts({
+  useEffect(() => {
+    setCarts(carts.map((cart) => ({
+      ...cart,
+      ...cartUtilities
+    })))
+  }, [])
+
+  const addCart = (name, props) => {
+    if (carts.find((cart) => cart.name === name)) throw new Error("Cannot create a new cart with duplicate `name` key.")
+    setCarts([
       ...carts,
-      [id]: createCart(name)
-    })
+      createCart(name)
+    ])
   }
-  const removeCart = (id) => {
-    const { [id]: removed, ...newCarts } = carts
-    setCarts(newCarts)
+  const removeCart = (name) => {
+    setCarts(carts.filter((cart) => cart.name !== name))
   }
   
   return (
     <ShoppingCartContext.Provider value={{
       carts, addCart, removeCart,
-      // activeCart, setActiveCart
+      activeCart, setActiveCart
      }}>
       { children }
     </ShoppingCartContext.Provider>
