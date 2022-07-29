461d43d02abcfe7a9d5d3c6c0cf4a2ed40819fa6
Griffin Roupe <groupe@renci.org>
2022-07-29 16:56:51 -0400

    Improves extra panel in cart checkout.

--- src/components/shopping-cart/shopping-cart.js
+++ src/components/shopping-cart/shopping-cart.js
@@ -1,6 +1,6 @@
 import { Fragment } from 'react'
 import { Space, Layout, Typography, Menu, Button, Checkbox, Dropdown } from 'antd'
-import { ShoppingCartOutlined, DeleteOutlined, DownOutlined } from '@ant-design/icons'
+import { ShoppingCartOutlined, DeleteOutlined, FolderAddOutlined, CopyOutlined, CaretDownOutlined } from '@ant-design/icons'
 import { CartList, useShoppingCart } from 'antd-shopping-cart'
 import './shopping-cart.css'
 
@@ -59,36 +59,67 @@ export const ShoppingCart = () => {
             }}
             renderExtra={ ({ selectedItems, setSelectedItems }) => {
               const selected = selectedItems.length > 0
+              const allSelected = selectedItems.length === activeCart.items.length
+              const indeterminateSelection = selected && !allSelected
+              const deselectAll = () => setSelectedItems([])
+              const selectAll = () => setSelectedItems(activeCart.items.map((item) => item.id))
               return (
                 <div style={{ display: "flex", alignItems: "center" }}>
                   <div className="selected-buttons" style={{ flex: 1 }}>
+                    <Button.Group>
+                      <Button
+                        className="selected-item-checkbox-button"
+                        type="text"
+                        size="large"
+                        style={{ padding: "0 4px" }}
+                        onClick={ () => {
+                          if (selected) deselectAll()
+                          else selectAll()
+                        } }
+                      >
+                        <Space>
+                          <Checkbox
+                            className="selected-item-checkbox"
+                            indeterminate={ indeterminateSelection }
+                            checked={ allSelected }
+                            />
+                        </Space>
+                      </Button>
+                      <Dropdown
+                        placement="topRight"
+                        trigger="click"
+                        overlay={ (
+                          <Menu
+                            items={[
+                              {
+                                label: "All",
+                                key: 1,
+                                onClick: selectAll
+                              },
+                              {
+                                label:  "None",
+                                key: 2,
+                                onClick: deselectAll
+                              },
+                              ...buckets.map((bucket) => ({
+                                  label: `${ bucket.name }`,
+                                  key: `all-${ bucket.id }`,
+                                  onClick: () => setSelectedItems(activeCart.items.filter((item) => item.bucketId === bucket.id).map((item) => item.id))
+                                }))
+                            ]}
+                          />
+                        ) }
+                      >
+                        <Button type="text" size="large" style={{ padding: "0 2px" }}>
+                          <CaretDownOutlined style={{ fontSize: 10, paddingBottom: 2 }} />
+                        </Button>
+                      </Dropdown>
+                    </Button.Group>
                     { selected && (
                       <Fragment>
-                        <Dropdown
-                          trigger="click"
-                          overlay={ <div>foobar</div> }
-                        >
-                          <Button.Group>
-                            <Button size="small" type="text">
-                              <Space>
-                                <Checkbox indeterminate />
-                                {/* <DownOutlined style={{ fontSize: 10 }} /> */}
-                              </Space>
-                            </Button>
-                            <Button size="small" type="text">
-                              <DownOutlined style={{ fontSize: 10 }} />
-                            </Button>
-                          </Button.Group>
-                        </Dropdown>
-                        <Button size="small" style={{ marginLeft:  8 }} icon={ <DeleteOutlined /> } onClick={ () => null }>
-                          Remove selected
-                        </Button>
-                        <Button size="small" style={{ marginLeft: 8 }} onClick={ () => null }>
-                          Move items
-                        </Button>
-                        <Button size="small" style={{ marginLeft: 8 }} onClick={ () => null }>
-                          Copy items
-                        </Button>
+                        <Button type="text" size="large" style={{ marginLeft: 8 }} icon={ <DeleteOutlined /> } onClick={ () => null } />
+                        <Button type="text" size="large" style={{ marginLeft: 8 }} icon={ <FolderAddOutlined /> } onClick={ () => null } />
+                        <Button type="text" size="large" style={{ marginLeft: 8 }} icon={ <CopyOutlined /> } onClick={ () => null } />
                       </Fragment>
                     ) }
                     </div>
