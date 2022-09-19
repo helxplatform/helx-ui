fa2891f31eb0873686b7ffc3adaab82030ff4f3f
Griffin Roupe <groupe@renci.org>
2022-09-19 18:43:20 -0400

    Improve debounced-input test

--- src/components/debounced-input.test.js
+++ src/components/debounced-input.test.js
@@ -12,22 +12,26 @@ const ExampleDebouncedInput = (props) => {
     )
 }
 
-it("DebouncedInput changes input value immediately", async () => {
-    const { queryByLabelText, getByLabelText, getByTestId } = render(<ExampleDebouncedInput />)
-    const input = getByTestId("example-debounced-input")
-    const valueText = getByTestId("example-debounced-value")
-    fireEvent.change(input, { target: { value: "test value" } })
-    expect(input.value).toBe("test value")
-
-})
-
-it("DebouncedInput changes value after debounce delay", async () => {
-    const { queryByLabelText, getByLabelText, getByTestId } = render(<ExampleDebouncedInput />)
-    const input = getByTestId("example-debounced-input")
-    const valueText = getByTestId("example-debounced-value")
-    fireEvent.change(input, { target: { value: "test value" } })
-    expect(input.textContent).not.toEqual("test value")
-    await waitFor(() => {
-        expect(valueText.textContent).toEqual("test value")
+describe("DebouncedInput", () => {
+    describe("when input changes", () => {
+        const delay = 250
+        let input, valueText
+        beforeEach(() => {
+            const { getByTestId } = render(<ExampleDebouncedInput debounce={ delay } />)
+            input = getByTestId("example-debounced-input")
+            valueText = getByTestId("example-debounced-value")
+            fireEvent.change(input, { target: { value: "test value" } })
+        })
+        it("the input text changes immediately", () => {
+            expect(input.value).toBe("test value")
+        })
+        it("value changes after debounce delay", async () => {
+            const startTime = Date.now()
+            expect(input.textContent).not.toEqual("test value")
+            // This will wait until either valueText updates to the debounced test or it times out.
+            await waitFor(() => expect(valueText.textContent).toEqual("test value"))
+            // Ensure that valueText does not update its value in less time than the debounced delay.
+            expect(Date.now() - startTime).toBeGreaterThanOrEqual(delay)
+        })
     })
 })
