006027d54a33b7031f1e9285ec2e4467cd2f7466
Griffin Roupe <groupe@renci.org>
2022-09-22 16:19:37 -0400

    Bug fixes to axios mocker and improvements

--- src/__mocks__/axios-mocker.js
+++ src/__mocks__/axios-mocker.js
@@ -23,62 +23,85 @@ export const setupAxiosMocker = () => {
                     eject: () => {}
                 }
             }
-            this._createResponse = function (statusCode, data, config) {
-                return {
-                    status: statusCode,
-                    statusText: undefined,
-                    data,
-                    headers: {},
-                    config,
-                    request: null
-                }
-            }
-            this.request = function(config={}) {
-                const requestConfig = {
-                    ...this.defaults,
-                    ...config
-                }
-                let { url, method, data, baseUrl } = requestConfig
-                url = baseUrl + url
-                const mock = mocks.find((mock) => (
-                    mock.config.method.toLowerCase() === method &&
-                    url.match(mock.config.url) &&
-                    (!mock.config.data || mock.config.data == data)
-                ))
-                if (mock) {
-                    const response = typeof mock.response === "function"
-                        ? mock.response(requestConfig)
-                        : mock.response
-                    return this._createResponse(response.status, response.data, requestConfig)
-                }
-                throw new Error(`Axios URL with config ${ JSON.stringify(config) } called but not matched by any mocks`)
-            }
-            Array.prototype.forEach.call(["delete", "get", "head", "options"], (method) => {
-                this[method] = function(url, config={}) {
-                    return this.request({
-                        url,
-                        method,
-                        ...config
-                    })
-                }
-            })
-            Array.prototype.forEach.call(["post", "put", "patch"], (method) => {
-                this[method] = function(url, data=undefined, config={}) {
-                    return this.request({
-                        url,
-                        method,
-                        data,
-                        ...config
-                    })
-                }
-            })
-            this.create = function(config) {
-                return new Axios(config)
+        }
+        Axios.prototype._createResponse = function (statusCode, data, config) {
+            return {
+                status: statusCode,
+                statusText: undefined,
+                data: JSON.parse(JSON.stringify(data)),
+                headers: {},
+                config,
+                request: null
             }
         }
-        return new Axios()
+        Axios.prototype.request = async function(config={}) {
+            const requestConfig = {
+                ...this.defaults,
+                ...config
+            }
+            let { url, method, data, baseUrl } = requestConfig
+            url = baseUrl + url
+            const mock = mocks.find((mock) => (
+                mock.config.method.toLowerCase() === method.toLowerCase() &&
+                url.match(mock.config.url) &&
+                (!mock.config.data || mock.config.data == data)
+            ))
+            if (mock) {
+                const response = typeof mock.response === "function"
+                    ? mock.response(requestConfig)
+                    : mock.response
+                return this._createResponse(response.status, response.data, requestConfig)
+            }
+            throw new Error(`Axios URL with config ${ JSON.stringify(config) } called but not matched by any mocks`)
+        }
+        Array.prototype.forEach.call(["delete", "get", "head", "options"], (method) => {
+            Axios.prototype[method] = function(url, config={}) {
+                return this.request({
+                    url,
+                    method,
+                    ...config
+                })
+            }
+        })
+        Array.prototype.forEach.call(["post", "put", "patch"], (method) => {
+            Axios.prototype[method] = function(url, data=undefined, config={}) {
+                return this.request({
+                    url,
+                    method,
+                    data,
+                    ...config
+                })
+            }
+        })
+        Axios.prototype.create = function(config) {
+            return new Axios(config)
+        }
+        function createInstance(config={}) {
+            const context = new Axios(config)
+            const instance = Axios.prototype.request.bind(context)
+            Object.keys(Axios.prototype).forEach((key) => {
+                const value = Axios.prototype[key]
+                instance[key] = value.bind(context)
+            })
+            Object.keys(context).forEach((key) => {
+                const value = context[key]
+                if (typeof value !== "function") {
+                    instance[key] = value
+                }
+            })
+            instance.create = function(config={}) {
+                return createInstance(config)
+            }
+            return instance
+        }
+        return createInstance({})
     })
     global.registerAxiosMock = ({ config, response }) => {
-        mocks.push({ config, response })
+        const mock = { config, response }
+        mocks.push(mock)
+        return mock
+    }
+    global.unregisterAxiosMock = (mock) => {
+        if (mocks.indexOf(mock) != -1) mocks.splice(mocks.indexOf(mock), 1)
     }
 }
