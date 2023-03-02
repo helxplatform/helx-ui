24c00e2b7e2dc1d38ba6337047bfe180fbf74cec
Griffin Roupe <groupe@renci.org>
2023-03-02 18:14:15 -0500

    doc: Adds comment to useTitle

--- src/views/view.tsx
+++ src/views/view.tsx
@@ -5,6 +5,12 @@ interface ViewProps {
     title: string | string[] | null
 }
 
+/** Notes:
+ * An empty string "" or empty array will just use the "Helx UI" component as the title.
+ * A null value will not update the title. It disables the hook. This may be useful in components
+ *  that wrap views conditionally, e.g. loading view that wraps other views.
+ * The "HeLx UI" component is appended to the end of all titles, does not need to be included.
+ */
 export const useTitle = (title: string | string[] | null) => {
     if (title === "") title = []
     const titleSegments = Array.isArray(title) ? title : [title]
