834b3a8ddd702b83e8d7abd62a217502f5e22cfe
Griffin Roupe <groupe@renci.org>
2023-04-19 14:23:58 -0400

    fix: Add missing import

--- src/views/splash-screen.js
+++ src/views/splash-screen.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Spin, Button } from "antd";
 import axios from "axios";
 import { callWithRetry } from "../utils";
