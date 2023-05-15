17a146a7af15f1e2f80aed0649188aaa9f96e198
Griffin Roupe <groupe@renci.org>
2023-05-15 13:39:13 -0400

    fix: Fix missing useCallback dependency

--- src/contexts/activity-context.js
+++ src/contexts/activity-context.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useCallback } from 'react'
 
 export const ActivityContext = createContext({});
 
