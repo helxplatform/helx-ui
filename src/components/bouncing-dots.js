36019025e8a408d4a018e98e1168e3b13862c637
Griffin Roupe <groupe@renci.org>
2023-06-16 16:47:17 -0400

    feat: Add count indicator to studies/cdes in result modal. Add bouncing dots component

---
+++ src/components/bouncing-dots.js
@@ -0,0 +1,27 @@
+export const BouncingDots = ({ height=8 }) => {
+    return (
+        // Functionality pulled from antd mobile library
+        <svg height={ `${ height }px` } viewBox="0 0 100 40" style={{ verticalAlign: "-0.125em" }}>
+            <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                <g transform="translate(-100.000000, -71.000000)">
+                    <g transform="translate(95.000000, 71.000000)">
+                        <g transform="translate(5.000000, 0.000000)">
+                            <rect fill="currentColor" x="20" y="16" width="8" height="8" rx="2">
+                                <animate attributeName="y" from="16" to="16" dur="2s" begin="0s" repeatCount="indefinite" values="16; 6; 26; 16; 16" keyTimes="0; 0.1; 0.3; 0.4; 1">
+                                </animate>
+                            </rect>
+                            <rect fill="currentColor" x="46" y="16" width="8" height="8" rx="2">
+                                <animate attributeName="y" from="16" to="16" dur="2s" begin="0.2s" repeatCount="indefinite" values="16; 6; 26; 16; 16" keyTimes="0; 0.1; 0.3; 0.4; 1">
+                                </animate>
+                            </rect>
+                            <rect fill="currentColor" x="72" y="16" width="8" height="8" rx="2">
+                                <animate attributeName="y" from="16" to="16" dur="2s" begin="0.4s" repeatCount="indefinite" values="16; 6; 26; 16; 16" keyTimes="0; 0.1; 0.3; 0.4; 1">
+                                </animate>
+                            </rect>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </svg>
+    )
+}
