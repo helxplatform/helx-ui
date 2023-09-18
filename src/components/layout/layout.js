31d8f23d5fa3912c71b42849a941081176ec0903
YaphetKG <45075777+YaphetKG@users.noreply.github.com>
2023-09-18 11:37:45 -0400

    Update layout.js

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -96,7 +96,7 @@ export const Layout = ({ children }) => {
       {
         context?.brand === 'heal'
         ? <Footer style={{ textAlign: 'center', paddingTop: 0 }}>
-          NIH HEAL Semantic Search is powered by Dug, an open source semantic search developed
+          HEAL Semantic Search is powered by Dug, an open source semantic search developed
           by <a href="https://renci.org" target="_blank" rel="noreferrer">RENCI</a> and <a href="https://www.rti.org/" target="_blank" rel="noreferrer">RTI International</a>
         </Footer>
         : <Footer style={{ textAlign: 'center', paddingTop: 0 }}>&copy;{ context?.meta.title ?? 'HeLx' }{new Date().getFullYear()}</Footer>
