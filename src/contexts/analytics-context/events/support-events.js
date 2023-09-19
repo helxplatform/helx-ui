d049f76458cc5793d441e26732bb2d1aca331c7e
Gaurav Vaidya <gaurav@renci.org>
2023-09-18 22:14:42 -0400

    First stab at adding updated HEAL support links.

--- src/contexts/analytics-context/events/support-events.js
+++ src/contexts/analytics-context/events/support-events.js
@@ -4,3 +4,17 @@ export function helpPortalOpened() {
         action: "support_help_portal_open"
     });
 }
+
+export function userGuideOpened() {
+    this.analytics.trackEvent({
+        category: "ui_interaction",
+        action: "support_user_guide_open"
+    });
+}
+
+export function faqsOpened() {
+    this.analytics.trackEvent({
+        category: "ui_interaction",
+        action: "support_faqs_open"
+    });
+}
