b3026733d3a0032017854aea0080ecbd020b894a
Gaurav Vaidya <gaurav@renci.org>
2023-09-19 15:07:36 -0400

    Restored "NIH" to footer.

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -95,7 +95,7 @@ export const Layout = ({ children }) => {
       {
         context?.brand === 'heal'
         ? <Footer style={{ textAlign: 'center', paddingTop: 0 }}>
-          HEAL Semantic Search is powered by Dug, an open source semantic search developed
+          NIH HEAL Semantic Search is powered by Dug, an open source semantic search developed
           by <a href="https://renci.org" target="_blank" rel="noopener noreferrer">RENCI</a> and <a href="https://www.rti.org/" target="_blank" rel="noopener noreferrer">RTI International</a>
         </Footer>
         : <Footer style={{ textAlign: 'center', paddingTop: 0 }}>&copy;{ context?.meta.title ?? 'HeLx' }{new Date().getFullYear()}</Footer>
