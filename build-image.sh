2054c679f3af79344eac0ce500c7f4651d1c4c82
PJ Linebaugh <pjl@renci.org>
2023-09-26 14:54:39 -0400

    fix: remove unneeded build script

--- build-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TIMESTAMP=`date "+%Y%m%d%H%M"`
-IMAGE_NAME="helxplatform/helx-ui"
-TAG=pjl-$TIMESTAMP-non-root
-REPO="containers.renci.org"
-
-docker build -t $IMAGE_NAME:$TAG -t $REPO/$IMAGE_NAME:$TAG .
